914c73f7c31f7d0b27d18260d54a3ad67dedf831
hashbrowns8 <145344361+hashbrowns8@users.noreply.github.com>
2025-12-06 14:18:51 +0900

    #4

--- core/designsystem/src/main/java/it/stamp/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/it/stamp/designsystem/theme/Theme.kt
@@ -2,8 +2,6 @@ package it.stamp.designsystem.theme
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Typography
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
 @Composable
