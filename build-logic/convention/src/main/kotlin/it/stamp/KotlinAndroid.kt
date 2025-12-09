19ebf87ec4ea568401968f42ae81eb5841a26348
hashbrowns8 <145344361+hashbrowns8@users.noreply.github.com>
2025-12-09 22:10:39 +0900

    add opt-in

--- build-logic/convention/src/main/kotlin/it/stamp/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/it/stamp/KotlinAndroid.kt
@@ -2,16 +2,28 @@ package it.stamp
 
 import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinBaseExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 import org.jetbrains.kotlin.gradle.dsl.kotlinExtension
 
-internal fun Project.configureKotlin() {
+internal inline fun <reified T: KotlinBaseExtension> Project.configureKotlin() = configure<T> {
     kotlinExtension.jvmToolchain(17)
+
+    when (this) {
+        is KotlinJvmProjectExtension -> compilerOptions
+        is KotlinAndroidProjectExtension -> compilerOptions
+        else -> throw Exception()
+    }.apply {
+        optIn.addAll("kotlin.time.ExperimentalTime")
+    }
 }
 
 internal fun Project.configureKotlinAndroid(
     commonExtension: CommonExtension<*, *, *, *, *, *>
 ) = with(commonExtension) {
-    configureKotlin()
+    configureKotlin<KotlinAndroidProjectExtension>()
 
     compileSdk {
         version = release(36)
