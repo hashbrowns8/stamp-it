c7452330241987770ba384b7ace22562eb362abd
hashbrowns8 <145344361+hashbrowns8@users.noreply.github.com>
2025-12-11 00:00:47 +0900

    Add Timber

--- build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -1,9 +1,7 @@
 
-import com.android.build.api.dsl.LibraryExtension
 import it.stamp.findLibrary
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 
 class AndroidFeatureConventionPlugin : Plugin<Project> {
@@ -16,28 +14,27 @@ class AndroidFeatureConventionPlugin : Plugin<Project> {
                 apply("org.jetbrains.kotlin.plugin.serialization")
             }
 
-            extensions.configure<LibraryExtension> {
-                dependencies {
-                    "implementation"(project(":core:domain"))
-                    "implementation"(project(":core:ui"))
-                    "implementation"(project(":core:designsystem"))
-                    "implementation"(platform(findLibrary("compose-bom")))
-                    "implementation"(findLibrary("compose-ui"))
-                    "implementation"(findLibrary("compose-ui-graphics"))
-                    "implementation"(findLibrary("compose-ui-tooling-preview"))
-                    "implementation"(findLibrary("compose-material3"))
-                    "implementation"(findLibrary("androidx-lifecycle-runtime-compose"))
-                    "implementation"(findLibrary("androidx-hilt-lifecycle-viewmodel-compose"))
-                    "implementation"(findLibrary("androidx-navigation3-runtime"))
-                    "implementation"(findLibrary("kotlinx-serialization-json"))
-                    "testImplementation"(findLibrary("junit"))
-                    "androidTestImplementation"(findLibrary("androidx-junit"))
-                    "androidTestImplementation"(findLibrary("androidx-espresso-core"))
-                    "androidTestImplementation"(platform(findLibrary("compose-bom")))
-                    "androidTestImplementation"(findLibrary("compose-ui-test-junit4"))
-                    "debugImplementation"(findLibrary("compose-ui-tooling"))
-                    "debugImplementation"(findLibrary("compose-ui-test-manifest"))
-                }
+            dependencies {
+                "implementation"(project(":core:domain"))
+                "implementation"(project(":core:ui"))
+                "implementation"(project(":core:designsystem"))
+                "implementation"(platform(findLibrary("compose-bom")))
+                "implementation"(findLibrary("compose-ui"))
+                "implementation"(findLibrary("compose-ui-graphics"))
+                "implementation"(findLibrary("compose-ui-tooling-preview"))
+                "implementation"(findLibrary("compose-material3"))
+                "implementation"(findLibrary("androidx-lifecycle-runtime-compose"))
+                "implementation"(findLibrary("androidx-hilt-lifecycle-viewmodel-compose"))
+                "implementation"(findLibrary("androidx-navigation3-runtime"))
+                "implementation"(findLibrary("kotlinx-serialization-json"))
+                "implementation"(findLibrary("timber"))
+                "testImplementation"(findLibrary("junit"))
+                "androidTestImplementation"(findLibrary("androidx-junit"))
+                "androidTestImplementation"(findLibrary("androidx-espresso-core"))
+                "androidTestImplementation"(platform(findLibrary("compose-bom")))
+                "androidTestImplementation"(findLibrary("compose-ui-test-junit4"))
+                "debugImplementation"(findLibrary("compose-ui-tooling"))
+                "debugImplementation"(findLibrary("compose-ui-test-manifest"))
             }
         }
     }
--- build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
@@ -1,9 +1,11 @@
 
 import com.android.build.api.dsl.LibraryExtension
 import it.stamp.configureAndroidLibrary
+import it.stamp.findLibrary
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
 
 class AndroidLibraryConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -14,6 +16,10 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
             }
 
             extensions.configure<LibraryExtension>(::configureAndroidLibrary)
+
+            dependencies {
+                "implementation"(findLibrary("timber"))
+            }
         }
     }
 }
