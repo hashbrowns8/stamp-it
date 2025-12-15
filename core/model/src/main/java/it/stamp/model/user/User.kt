2930afa684211a9fa784bee4152f809e227d8a0d
hashbrowns8 <145344361+hashbrowns8@users.noreply.github.com>
2025-12-15 23:18:41 +0900

    Rename fields

--- core/model/src/main/java/it/stamp/model/user/User.kt
+++ core/model/src/main/java/it/stamp/model/user/User.kt
@@ -4,6 +4,6 @@ import it.stamp.model.ids.UserId
 
 data class User(
     val id: UserId,
-    val nickname: String,
-    val avatarUrl: String?,
+    val displayName: String,
+    val avatar: String?,
 )
