a145c3622eae671dfe796d9a622a0e0228251e10
hashbrowns8 <145344361+hashbrowns8@users.noreply.github.com>
2025-12-16 00:37:41 +0900

    Add FirebaseConventionPlugin

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.stampit.android.library)
     alias(libs.plugins.stampit.android.hilt)
+    alias(libs.plugins.stampit.firebase)
 }
 
 android.namespace = "it.stamp.data"
@@ -8,11 +9,7 @@ android.namespace = "it.stamp.data"
 dependencies {
     implementation(projects.core.domain)
     implementation(projects.core.model)
-
-    implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
     implementation(libs.coroutines.play.services)
-    implementation("androidx.credentials:credentials:1.3.0")
-    implementation("androidx.credentials:credentials-play-services-auth:1.3.0")
-    implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
 }
