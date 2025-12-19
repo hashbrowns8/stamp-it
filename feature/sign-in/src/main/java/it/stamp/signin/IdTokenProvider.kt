a7143bf4a448925e335636dda89110cebb066d54
hashbrowns8 <145344361+hashbrowns8@users.noreply.github.com>
2025-12-20 03:18:50 +0900

    Add IdTokenProvider

---
+++ feature/sign-in/src/main/java/it/stamp/signin/IdTokenProvider.kt
@@ -0,0 +1,33 @@
+package it.stamp.signin
+
+import android.content.Context
+import androidx.credentials.CredentialManager
+import androidx.credentials.GetCredentialRequest
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+
+interface IdTokenProvider {
+    suspend fun getIdToken(activityContext: Context): Result<String>
+}
+
+class GoogleIdTokenProvider(
+    private val credentialManager: CredentialManager,
+) : IdTokenProvider {
+    override suspend fun getIdToken(activityContext: Context): Result<String> = runCatching {
+        val credentialOption = GetGoogleIdOption.Builder()
+            .setServerClientId(BuildConfig.GOOGLE_SERVER_CLIENT_ID)
+            .setFilterByAuthorizedAccounts(false)
+            .setAutoSelectEnabled(true)
+            .build()
+
+        val request = GetCredentialRequest.Builder()
+            .addCredentialOption(credentialOption)
+            .build()
+
+        val result = credentialManager.getCredential(activityContext, request)
+
+        val credential = GoogleIdTokenCredential.createFrom(result.credential.data)
+
+        return@runCatching credential.idToken
+    }
+}
