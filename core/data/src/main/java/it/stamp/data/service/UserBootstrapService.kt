f68fb4201211885df1d7e597574a32cd54901cd5
hashbrowns8 <145344361+hashbrowns8@users.noreply.github.com>
2025-12-20 03:28:07 +0900

    Update UserBootstrapService.kt

--- core/data/src/main/java/it/stamp/data/service/UserBootstrapService.kt
+++ core/data/src/main/java/it/stamp/data/service/UserBootstrapService.kt
@@ -1,12 +1,12 @@
 package it.stamp.data.service
 
 import com.google.firebase.firestore.FirebaseFirestore
-import it.stamp.data.model.toFirestoreGroup
-import it.stamp.data.model.toFirestoreMembership
-import it.stamp.data.model.toFirestoreUser
-import it.stamp.data.util.groupDocument
-import it.stamp.data.util.membershipDocument
-import it.stamp.data.util.userDocument
+import it.stamp.data.firestore.model.FirestoreGroup
+import it.stamp.data.firestore.model.FirestoreMembership
+import it.stamp.data.firestore.model.FirestoreUser
+import it.stamp.data.firestore.util.groupDocument
+import it.stamp.data.firestore.util.membershipDocument
+import it.stamp.data.firestore.util.userDocument
 import it.stamp.domain.service.UserBootstrapService
 import it.stamp.model.membership.Group
 import it.stamp.model.membership.Membership
@@ -25,14 +25,43 @@ class FirestoreUserBootstrapService @Inject constructor(
     ): Result<User> = runCatching {
         firestore.runBatch { batch ->
             with(batch) {
-                set(firestore.userDocument(user.id.value), user.toFirestoreUser(group.id))
+                val userFirestore = FirestoreUser(
+                    userId = user.id.value,
+                    groupId = group.id.value,
+                    nickname = user.displayName,
+                    profileImage = user.avatar.ifEmpty {
+                        DEFAULT_PROFILE_IMAGE
+                    },
+                )
 
-                set(firestore.groupDocument(group.id.value), group.toFirestoreGroup(user.id))
+                set(firestore.userDocument(user.id.value), userFirestore)
 
-                set(firestore.membershipDocument(membership.id.value), membership.toFirestoreMembership(user.displayName, user.avatar))
+                val groupFirestore = FirestoreGroup(
+                    groupId = group.id.value,
+                    leaderId = user.id.value,
+                    name = group.name,
+                    inviteCode = group.inviteCode.value,
+                )
+
+                set(firestore.groupDocument(group.id.value), groupFirestore)
+
+                val membershipFirestore = FirestoreMembership(
+                    membershipId = membership.id.value,
+                    groupId = group.id.value,
+                    userId = user.id.value,
+                    isLeader = membership.isLeader,
+                    nickname = user.displayName,
+                    profileImage = user.avatar,
+                )
+
+                set(firestore.membershipDocument(membership.id.value), membershipFirestore)
             }
         }.await()
 
         return@runCatching user
+    }
+
+    companion object {
+        private const val DEFAULT_PROFILE_IMAGE = "profileImage1"
     }
 }
