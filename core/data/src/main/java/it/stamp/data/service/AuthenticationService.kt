bc440d6467204f40f23fb68063076d87dc7f4789
hashbrowns8 <145344361+hashbrowns8@users.noreply.github.com>
2025-12-20 03:28:01 +0900

    Update AuthenticationService.kt

--- core/data/src/main/java/it/stamp/data/service/AuthenticationService.kt
+++ core/data/src/main/java/it/stamp/data/service/AuthenticationService.kt
@@ -1,71 +1,41 @@
 package it.stamp.data.service
 
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.snapshots
-import com.google.firebase.firestore.toObject
-import it.stamp.data.model.FirestoreUser
-import it.stamp.data.model.toDomainUser
-import it.stamp.data.util.user
-import it.stamp.data.util.userDocument
+import it.stamp.data.firestore.mapper.UserMapper
+import it.stamp.data.firestore.source.FirebaseAuthenticationProvider
+import it.stamp.data.firestore.source.UserFirestoreDataSource
 import it.stamp.domain.service.AuthenticationService
 import it.stamp.model.authentication.AuthenticationResult
 import it.stamp.model.user.User
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 class FirebaseAuthenticationService @Inject constructor(
-    private val auth: FirebaseAuth,
-    private val firestore: FirebaseFirestore
+    private val authenticationProvider: FirebaseAuthenticationProvider,
+    private val userDataSource: UserFirestoreDataSource,
 ) : AuthenticationService {
 
-    @OptIn(ExperimentalCoroutinesApi::class)
-    override val user: Flow<User?> = callbackFlow {
-        val listener = FirebaseAuth.AuthStateListener {
-            trySend(auth.currentUser)
-        }
+    override val user: Flow<User?> = authenticationProvider.user
+        .flatMapLatest { userFirebase ->
+            if (userFirebase == null) return@flatMapLatest flowOf(null)
 
-        auth.addAuthStateListener(listener)
-
-        trySend(auth.currentUser)
-
-        awaitClose {
-            auth.removeAuthStateListener(listener)
-        }
-    }.flatMapLatest { user ->
-        if (user == null) {
-            flowOf(null)
-        } else {
-            firestore.userDocument(user.uid)
-                .snapshots()
-                .map {
-                    it.toObject<FirestoreUser>()
-                        ?.toDomainUser()
-                        ?: user.toDomainUser()
+            userDataSource.observe(userFirebase.uid)
+                .map { userFirestore ->
+                    userFirestore
+                        ?.let(UserMapper::toDomainModel)
+                        ?: UserMapper.toDomainModel(userFirebase)
                 }
         }
-    }.flowOn(Dispatchers.IO)
 
     override suspend fun signInWithGoogle(idToken: String): AuthenticationResult = runCatching {
-        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        val userFirebase = authenticationProvider.signInWithGoogle(idToken)
 
-        val result = auth.signInWithCredential(credential).await()
+        val userFirestore = userDataSource.read(userFirebase.uid)
 
-        val userFirebase = result.user ?: throw IllegalStateException("Firebase User is Null :-(")
-
-        val userFirestore = firestore.user(userFirebase.uid)
-
-        val user = userFirestore ?: userFirebase.toDomainUser()
+        val user = userFirestore?.let(UserMapper::toDomainModel)
+            ?: UserMapper.toDomainModel(userFirebase)
 
         val isNewUser = userFirestore == null // needs Bootstrap
 
@@ -74,7 +44,7 @@ class FirebaseAuthenticationService @Inject constructor(
         AuthenticationResult.Failure(throwable)
     }
 
-    override fun signOut(): Result<Unit> = runCatching {
-        auth.signOut()
+    override fun signOut() {
+        authenticationProvider.signOut()
     }
 }
